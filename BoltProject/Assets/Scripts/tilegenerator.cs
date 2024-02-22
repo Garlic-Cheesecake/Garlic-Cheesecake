c759e0b1224e1318d4e782baf1aa0a5c1cf338ed
CrimsonRuby11 <skprithviraj8@gmail.com>
2024-02-22 16:19:49 +0530

    Update tilegen

--- BoltProject/Assets/Scripts/tilegenerator.cs
+++ BoltProject/Assets/Scripts/tilegenerator.cs
@@ -1,20 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class tilegenerator : MonoBehaviour
 {
     [SerializeField]private tile[] tiles = new tile[4];
 
+    private string[] attributes = new string[4];
+    private int[] a = new int[4];
+
 
     // Start is called before the first frame update
     void Start()
     {   
+        int i = 0;
+        while(i < 4) {
+            a[i] = i;
+            i++;
+        }
+
+        int[] b =  a.OrderBy(x => Random.value).ToArray();
+        i = 0;
+        while(i < 4) {
+            Debug.Log(b[i]);
+            i++;
+        }
+
         tiles[0].pressKey = KeyCode.A;
         tiles[1].pressKey = KeyCode.S;
         tiles[2].pressKey = KeyCode.D;
         tiles[3].pressKey = KeyCode.F;
+
+        attributes[0] = "First attribute";
+        attributes[1] = "Second attribute";
+        attributes[2] = "Third attribute";
+        attributes[3] = "Fourth attribute";
+
+        i = 0;
+        foreach(tile t in tiles) {
+            t.updateText(attributes[b[i]]);
+            if(i == 0 || i == 1) {
+                t.setCorrect(true);
+            }
+            i++;
+        }
 
         readFromFile();
     }
